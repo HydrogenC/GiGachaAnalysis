ead9a6cee8bf4a4b6abd2be2707c6b9be4eb17d6
ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>
2022-11-08 22:22:37 +0800

    Add date analysis

--- GiGachaAnalysis/Program.cs
+++ GiGachaAnalysis/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
+using System.Reflection.Emit;
 using ScottPlot;
+using static ScottPlot.Plottable.PopulationPlot;
 
 namespace GiGachaAnalysis;
 
@@ -32,6 +35,90 @@ internal class Program
         "刻晴", "迪卢克", "七七", "莫娜", "琴", "提纳里"
     };
 
+    const int DAY_SLICES = 48;
+    const int SLICE_MINUTES = 24 * 60 / DAY_SLICES;
+
+    static void CreateTimePlot(PullStats[] timeStats)
+    {
+        var timeAvgPlot = new Plot(1000, 600);
+        var dataRange = Enumerable.Range(0, DAY_SLICES);
+
+        var avgData = timeAvgPlot.AddBar(
+            dataRange.Select((x) => timeStats[x].PityAvgSpecial).ToArray(),
+            dataRange.Select((x) => (x + 0.5) * SLICE_MINUTES / 60.0).ToArray()
+            );
+        avgData.XAxisIndex = 0;
+        avgData.YAxisIndex = 0;
+        avgData.BarWidth = 0.8 * SLICE_MINUTES / 60.0;
+
+        var chanceData = timeAvgPlot.AddScatter(
+            dataRange.Select((x) => x * SLICE_MINUTES / 60.0).ToArray(),
+            dataRange.Select((x) => timeStats[x].WinChance).ToArray()
+            );
+        chanceData.XAxisIndex = 0;
+        chanceData.YAxisIndex = 1;
+
+        timeAvgPlot.Title("Genshin Pulls Analysis");
+        timeAvgPlot.XAxis.Label("Time");
+        timeAvgPlot.YAxis.Label("Avg Pulls");
+        timeAvgPlot.YAxis.Color(avgData.Color);
+        timeAvgPlot.YAxis2.Ticks(true);
+        timeAvgPlot.SetAxisLimits(yMin: 80, yMax: 95, yAxisIndex: 0);
+        timeAvgPlot.SetAxisLimits(yMin: 0.4, yMax: 0.6, yAxisIndex: 1);
+        timeAvgPlot.YAxis2.Label("Winning Chance");
+        timeAvgPlot.YAxis2.Color(chanceData.Color);
+
+        timeAvgPlot.SaveFig("D:\\TimePlot.png");
+    }
+
+    static void CreateDatePlot(PullStats[] dateStats, DateTime[] dates)
+    {
+        var dateTotalPlot = new Plot(1000, 600);
+        var dataRange = Enumerable.Range(0, dates.Length);
+
+        var countData = dateTotalPlot.AddScatter(
+            dataRange.Select((x) => dates[x].ToOADate()).ToArray(),
+            dataRange.Select((x) => (double)dateStats[x].SpecialCount).ToArray()
+            );
+        countData.XAxisIndex = 0;
+        countData.YAxisIndex = 0;
+
+        var peaks = new List<int>();
+        for (int i = 0; i < dateStats.Length; i++)
+        {
+            if (dateStats[i].SpecialCount >= 1000)
+            {
+                peaks.Add(i);
+            }
+        }
+
+        dateTotalPlot.XAxis.ManualTickPositions(
+            peaks.Select((x) => dates[x].ToOADate()).ToArray(),
+            peaks.Select((x) => dates[x].ToString("d")).ToArray()
+            );
+
+        dateTotalPlot.Title("Genshin Pulls Analysis");
+        dateTotalPlot.XAxis.TickLabelStyle(rotation: 45);
+        dateTotalPlot.XAxis.Label("Time");
+        dateTotalPlot.XAxis.DateTimeFormat(true);
+        dateTotalPlot.YAxis.Label("Total Special");
+        dateTotalPlot.YAxis.Color(countData.Color);
+
+        dateTotalPlot.SaveFig("D:\\DatePlot.png");
+    }
+
+    static void AddToAvg(ref PullStats stats, Pull pull)
+    {
+        stats.PityAvg = (stats.PityAvg * stats.Count + pull.Pities) / (stats.Count + 1.0);
+        stats.Count++;
+
+        if (pull.IsSpecial)
+        {
+            stats.WinChance = (stats.WinChance * stats.SpecialCount + (pull.WonFifty ? 1 : 0)) / (stats.SpecialCount + 1.0);
+            stats.PityAvgSpecial = (stats.PityAvgSpecial * stats.SpecialCount + pull.PitiesSpecial) / (stats.SpecialCount + 1.0);
+            stats.SpecialCount++;
+        }
+    }
     static void Main(string[] args)
     {
         CultureInfo provider = CultureInfo.InvariantCulture;
@@ -83,8 +170,6 @@ internal class Program
             }
         }
 
-        const int DAY_SLICES = 48;
-        const int SLICE_MINUTES = 24 * 60 / DAY_SLICES;
         var timeStats = new PullStats[DAY_SLICES];
         for (int i = 0; i < DAY_SLICES; i++)
         {
@@ -95,46 +180,29 @@ internal class Program
         {
             var time = i.Time.TimeOfDay;
             var index = (time.Hours * 60 + time.Minutes) / SLICE_MINUTES;
-
-            timeStats[index].PityAvg = (timeStats[index].PityAvg * timeStats[index].Count + i.Pities) / (timeStats[index].Count + 1.0);
-            timeStats[index].Count++;
-
-            if (i.IsSpecial)
-            {
-                timeStats[index].WinChance = (timeStats[index].WinChance * timeStats[index].SpecialCount + (i.WonFifty ? 1 : 0)) / (timeStats[index].SpecialCount + 1.0);
-                timeStats[index].PityAvgSpecial = (timeStats[index].PityAvgSpecial * timeStats[index].SpecialCount + i.PitiesSpecial) / (timeStats[index].SpecialCount + 1.0);
-                timeStats[index].SpecialCount++;
-            }
+            AddToAvg(ref timeStats[index], i);
         }
 
-        var timeAvgPlot = new Plot(1000, 600);
-        var dataRange = Enumerable.Range(0, DAY_SLICES);
+        CreateTimePlot(timeStats);
 
-        var avgData = timeAvgPlot.AddBar(
-            dataRange.Select((x) => timeStats[x].PityAvgSpecial).ToArray(),
-            dataRange.Select((x) => x * SLICE_MINUTES / 60.0).ToArray()
-            );
-        avgData.XAxisIndex = 0;
-        avgData.YAxisIndex = 0;
-        avgData.BarWidth = 0.8 * SLICE_MINUTES / 60.0;
+        var minDate = new DateTime(pulls.Min((x) => x.Time.Ticks)).Date;
+        var maxDate = new DateTime(pulls.Max((x) => x.Time.Ticks)).Date;
 
-        var chanceData = timeAvgPlot.AddScatter(
-            dataRange.Select((x) => x * SLICE_MINUTES / 60.0).ToArray(),
-            dataRange.Select((x) => timeStats[x].WinChance).ToArray()
-            );
-        chanceData.XAxisIndex = 0;
-        chanceData.YAxisIndex = 1;
+        Console.WriteLine($"Date vary from {minDate} to {maxDate}");
+        var k = (maxDate - minDate).Days + 1;
+        var dates = new DateTime[k];
+        var dateStats = new PullStats[k];
+        for (int i = 0; i < k; i++)
+        {
+            dates[i] = minDate.AddDays(i).Date;
+        }
 
-        timeAvgPlot.Title("Genshin Pulls Analysis");
-        timeAvgPlot.XAxis.Label("Time");
-        timeAvgPlot.YAxis.Label("Avg Pulls");
-        timeAvgPlot.YAxis.Color(avgData.Color);
-        timeAvgPlot.YAxis2.Ticks(true);
-        timeAvgPlot.SetAxisLimits(yMin: 80, yMax: 95, yAxisIndex: 0);
-        timeAvgPlot.SetAxisLimits(yMin: 0.4, yMax: 0.6, yAxisIndex: 1);
-        timeAvgPlot.YAxis2.Label("Winning Chance");
-        timeAvgPlot.YAxis2.Color(chanceData.Color);
+        foreach (var i in pulls)
+        {
+            var index = (i.Time.Date - minDate).Days;
+            AddToAvg(ref dateStats[index], i);
+        }
 
-        timeAvgPlot.SaveFig("D:\\TimePlot.png");
+        CreateDatePlot(dateStats, dates);
     }
 }
